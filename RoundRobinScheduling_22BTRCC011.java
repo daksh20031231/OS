c1f058db1834301af08191902a1ffba23444bb3c
daksh20031231 <69597236+daksh20031231@users.noreply.github.com>
2023-05-18 23:27:26 +0530

    Add files via upload

---
+++ RoundRobinScheduling_22BTRCC011.java
@@ -0,0 +1,104 @@
+import java.util.*;
+
+class Process {
+    int pid; // Process ID
+    int bt; // Burst Time
+    int wt; // Waiting Time
+    int tat; // Turnaround Time
+
+    public Process(int pid, int bt) {
+        this.pid = pid;
+        this.bt = bt;
+        this.wt = 0;
+        this.tat = 0;
+    }
+}
+
+public class RoundRobinScheduling_22BTRCC011 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of processes: ");
+        int n = scanner.nextInt();
+
+        System.out.print("Enter the time quantum: ");
+        int quantum = scanner.nextInt();
+
+        List<Process> processes = new ArrayList<>();
+
+        // Input burst time for each process
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter burst time for process " + (i + 1) + ": ");
+            int burstTime = scanner.nextInt();
+            processes.add(new Process(i + 1, burstTime));
+        }
+
+        // Calculate waiting time and turnaround time
+        calculateTimes(processes, quantum);
+
+        // Display the results
+        displayResults(processes);
+
+        scanner.close();
+    }
+
+    public static void calculateTimes(List<Process> processes, int quantum) {
+        int n = processes.size();
+        int[] remainingBurstTime = new int[n];
+
+        // Initialize remaining burst time of processes
+        for (int i = 0; i < n; i++) {
+            remainingBurstTime[i] = processes.get(i).bt;
+        }
+
+        int currentTime = 0; // Current time
+
+        // Run the round robin scheduling algorithm
+        while (true) {
+            boolean allProcessesCompleted = true;
+
+            for (int i = 0; i < n; i++) {
+                if (remainingBurstTime[i] > 0) {
+                    allProcessesCompleted = false;
+
+                    if (remainingBurstTime[i] > quantum) {
+                        currentTime += quantum;
+                        remainingBurstTime[i] -= quantum;
+                    } else {
+                        currentTime += remainingBurstTime[i];
+                        processes.get(i).wt = currentTime - processes.get(i).bt;
+                        remainingBurstTime[i] = 0;
+                    }
+                }
+            }
+
+            if (allProcessesCompleted) {
+                break;
+            }
+        }
+
+        // Calculate turnaround time for each process
+        for (int i = 0; i < n; i++) {
+            processes.get(i).tat = processes.get(i).wt + processes.get(i).bt;
+        }
+    }
+
+    public static void displayResults(List<Process> processes) {
+        System.out.println("\nProcess\tBurst Time\tWaiting Time\tTurnaround Time");
+
+        int totalWaitingTime = 0;
+        int totalTurnaroundTime = 0;
+
+        for (Process process : processes) {
+            System.out.println(process.pid + "\t\t" + process.bt + "\t\t" + process.wt + "\t\t" + process.tat);
+            totalWaitingTime += process.wt;
+            totalTurnaroundTime += process.tat;
+        }
+
+        double averageWaitingTime = (double) totalWaitingTime / processes.size();
+        double averageTurnaroundTime = (double) totalTurnaroundTime / processes.size();
+
+        System.out.println("\nAverage Waiting Time: " + averageWaitingTime);
+        System.out.println("Average Turnaround Time: " + averageTurnaroundTime);
+    }
+}
---
+++ lab_2.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class lab_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of processes: ");
+        int n = sc.nextInt();
+
+        int[] burstTime = new int[n];
+        int[] waitingTime = new int[n];
+        int[] turnaroundTime = new int[n];
+
+        System.out.println("\nEnter the burst time for each process: ");
+        for(int i = 0; i < n; i++) {
+            System.out.print("Process " + (i+1) + ": ");
+            burstTime[i] = sc.nextInt();
+        }
+
+        // Sorting the burst time in ascending order
+        for(int i = 0; i < n-1; i++) {
+            for(int j = 0; j < n-i-1; j++) {
+                if(burstTime[j] > burstTime[j+1]) {
+                    int temp = burstTime[j];
+                    burstTime[j] = burstTime[j+1];
+                    burstTime[j+1] = temp;
+                }
+            }
+        }
+
+        waitingTime[0] = 0;
+        for(int i = 1; i < n; i++) {
+            waitingTime[i] = waitingTime[i-1] + burstTime[i-1];
+        }
+
+        for(int i = 0; i < n; i++) {
+            turnaroundTime[i] = waitingTime[i] + burstTime[i];
+        }
+
+        float totalWaitingTime = 0, totalTurnaroundTime = 0;
+        for(int i = 0; i < n; i++) {
+            totalWaitingTime += waitingTime[i];
+            totalTurnaroundTime += turnaroundTime[i];
+        }
+
+        System.out.println("\nSJF Scheduling: ");
+        System.out.println("Process\tBurst Time\tWaiting Time\tTurnaround Time");
+        for(int i = 0; i < n; i++) {
+            System.out.println((i+1) + "\t" + burstTime[i] + "\t\t" + waitingTime[i] + "\t\t" + turnaroundTime[i]);
+        }
+
+        System.out.println("\nAverage Waiting Time: " + (totalWaitingTime/n));
+        System.out.println("Average Turnaround Time: " + (totalTurnaroundTime/n));
+
+        sc.close();
+    }
+}
